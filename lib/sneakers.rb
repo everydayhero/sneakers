9794eaa7fa674121c069bcf3747b117a816888c1
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2015-05-22 17:36:11 +1000

    Add donation builder

--- lib/sneakers.rb
+++ lib/sneakers.rb
@@ -4,6 +4,7 @@ module Sneakers
 end
 
 require "sneakers/order"
+require "sneakers/donation_builder"
 require "sneakers/signature"
 require "sneakers/application"
 require "sneakers/signed_applications"
